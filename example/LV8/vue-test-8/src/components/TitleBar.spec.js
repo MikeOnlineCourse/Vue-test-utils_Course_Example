539b84d80953c3ab7287ea22629151e78fabd30b
mike <a3804430@hotmail.com>
2022-03-01 11:42:36 +0800

    add new Example lv8

---
+++ example/LV8/vue-test-8/src/components/TitleBar.spec.js
@@ -0,0 +1,16 @@
+
+import { mount } from '@vue/test-utils'
+import TitleBar from './TitleBar.vue'
+
+describe("TitleBar.vue", () => {
+it('renders slots content', () => {
+  const wrapper = mount(TitleBar, {
+    slots: {
+      default: 'Default',
+      detail: '這是預設的內容嗎?'
+    }
+  })
+  expect(wrapper.find(".title").html()).toBe('<h1 class="title">Default</h1>')
+  expect(wrapper.find(".detail").html()).toBe('<p class="detail">這是預設的內容嗎?</p>')
+})
+})
