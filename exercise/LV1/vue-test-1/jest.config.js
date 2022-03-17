1fe9e5c4843cbd46e5cf056a5fe3b7ce41624f67
Mike <a3804430@hotmail.com>
2022-03-17 20:00:18 +0800

    fix: lv1

--- exercise/LV1/vue-test-1/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  testMatch: ["**/src/**/*.spec.[jt]s?(x)"],
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.vue$": "@vue/vue3-jest"
-  },
-};
