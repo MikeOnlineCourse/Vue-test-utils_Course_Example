81cc1f14f5198b2fa5173ee9ebb6ddf2deb45e69
Mike <a3804430@hotmail.com>
2022-04-03 11:00:14 +0800

    fix exercise

--- exercise/LV6/vue-test-6/src/components/MenuSlid.spec.js
+++ exercise/LV6/vue-test-6/src/components/MenuSlid.spec.js
@@ -1,18 +1,8 @@
 import { shallowMount } from "@vue/test-utils";
-import { createStore } from "vuex";
-import { store } from "@/store";
 import MenuSlid from "./MenuSlid.vue";
 
 describe("MenuBtn.vue", () => {
-  it("test Vuex action change state", async () => {
-    const wrapper = shallowMount(MenuSlid, {
-      global: {
-        plugins: [createStore(store)],
-      },
-    });
-
-    expect(wrapper.find(".menu").classes().includes("open")).toBe(false);
-    await wrapper.find(".closeBtn").trigger("click");
-    expect(wrapper.find(".menu").classes().includes("open")).toBe(true);
+  it("test Vuex action change state", () => {
+    const wrapper = shallowMount(MenuSlid);
   });
 });
